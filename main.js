31b3d7b34dadd80049444ae3e996c08b93055b2f
assortest <2861854011@qq.com>
2026-02-11 20:28:05 +0800

    更新进程

--- main.js
+++ main.js
@@ -39,6 +39,7 @@ try {
     6985: "NBA2K26.exe", //NBA2k26
     1693: "EternalReturn.exe", //永恒轮回
     6546: "BlueArchive.exe", //蔚蓝档案
+    8538:"Nioh3.exe",//仁王3
   };
   const ExcludedGameIDs = [109, 437, 1544, 274, 1921, 1342, 860]; //steam epic 暴雪 育碧uplay eaapp  rockstar GOG
   const UI_STATES = {
